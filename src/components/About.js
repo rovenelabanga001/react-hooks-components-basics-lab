94861b3c53a1b60da1ffb4a0b376e1cc993d2a08
rovenelabanga <rovenelabanga2001@gmail.com>
2024-10-29 20:00:27 +0300

    creates components and adds them to the main component

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About(){
+    return (
+        <div id="about">
+            <h2>About</h2>
+        </div>
+    )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+    return (
+      <div id="home">
+        <h1>Home</h1>
+      </div>
+    );
+  }
